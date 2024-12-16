66dd1ca61560dc2c8cf432899807ff498830a4a2
Enrico.Schuetz <enrico.schuetz@evosoft.com>
2024-12-16 17:42:37 +0100

    init

--- docker/download-cli.sh
+++ docker/download-cli.sh
@@ -3,4 +3,4 @@
 URL=$(curl -s https://api.github.com/repos/enricoschuetz/tmc-clone/releases/latest | jq -r '.assets | .[] | select(.name == "tmc-linux-amd64") | .browser_download_url')
 
 curl -OL $URL
-mv ./tmc-linux-amd64 ./tmc
+mv ./tmc-clone-linux-amd64 ./tmc-clone
